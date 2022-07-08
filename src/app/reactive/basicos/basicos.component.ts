588adce575d611dfb58559c1419e1a59a5902146
mateoU1297 <castro.mateo@correounivalle.edu.co>
2022-07-07 23:51:00 -0500

    se termina reactivos básicos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre      : [ , [Validators.required, Validators.minLength(3)]],
+    precio      : [ , [Validators.required, Validators.min(0)]],
+    existencias : [ , [Validators.required, Validators.min(0)]],
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  campoEsValido(campo: string): any {
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched;
+  }
 
   ngOnInit(): void {
+    this.miFormulario.setValue({
+      nombre      : 'RTX 4080ti',
+      precio      : 1600,
+      existencias : 1000
+    });
+  }
+
+  guardar(): void {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    this.miFormulario.reset();
+
   }
 
 }
